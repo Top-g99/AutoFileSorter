fd3a696b543d0adb9a11468916fa9a0ada08910e
Ishaan khan <khanishaan846@gmail.com>
2025-05-09 23:52:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+FILE_TYPES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp"],
+    "Documents": [".pdf", ".docx", ".txt", ".xlsx"],
+    "Videos": [".mp4", ".mov", ".avi"],
+    "Archives": [".zip", ".rar", ".tar"],
+    "Scripts": [".py", ".js", ".html", ".css"],
+}
